c2b5f0fe137f15d9f700141a220297fe9e7c799b
jdquicenp <57123064+jdquicenp@users.noreply.github.com>
2024-11-26 13:12:23 -0500

    Update recepcion_facturas.py

--- Recepcion_Facturas/models/recepcion_facturas.py
+++ Recepcion_Facturas/models/recepcion_facturas.py
@@ -3,23 +3,18 @@ from odoo.exceptions import UserError
 
 class XRecepcionFacturas(models.Model):
     _name = 'x_recepcion_facturas'
-    _inherit = ['mail.thread', 'mail.activity.mixin']  # Inheriting to use messages and attachments
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
     name = fields.Char(string='Factura')
     x_studio_validar = fields.Boolean(string="Validar", track_visibility='onchange')
-    
-    @api.model
-    def create(self, vals):
-        record = super(XRecepcionFacturas, self).create(vals)
-        record._check_attachments()
-        return record
 
-    @api.onchange('x_studio_validar')
-    def _onchange_x_studio_validar(self):
-        # Verifica si hay adjuntos cuando cambia el campo x_studio_validar
-        raise UserError('chequear')
-        if self.x_studio_validar:
-            self._check_attachments()
+    def write(self, values):
+        """ Sobrescribir el método write para verificar los adjuntos cuando el campo 'x_studio_validar' cambie """
+        if 'x_studio_validar' in values:
+            # Verifica si el valor de 'x_studio_validar' ha cambiado
+            if values['x_studio_validar']:
+                self._check_attachments()
+        return super(XRecepcionFacturas, self).write(values)
 
     def _check_attachments(self):
         """ Verifica si hay archivos adjuntos en los mensajes internos del registro """
@@ -32,7 +27,6 @@ class XRecepcionFacturas(models.Model):
             raise UserError('No se encontraron adjuntos en los mensajes internos.')
 
         # Si quieres hacer algo más con los archivos adjuntos, puedes agregarlos aquí
-        # Por ejemplo, descomprimir archivos zip o procesar archivos XML
         for attachment in attachments:
             if attachment.mimetype == 'application/zip':
                 self._process_zip_attachment(attachment)
