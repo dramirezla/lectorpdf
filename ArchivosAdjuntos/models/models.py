33c26e5f1acd5299bca9c53294acedbbc5683b04
jdquicenp <57123064+jdquicenp@users.noreply.github.com>
2024-11-27 19:11:22 -0500

    Update models.py

--- ArchivosAdjuntos/models/models.py
+++ ArchivosAdjuntos/models/models.py
@@ -4,13 +4,17 @@ class ListAttachmentLoader(models.Model):
     _name = "x_recepcion_facturas"
     _inherit = "x_recepcion_facturas"
 
-    x_studio_adjunto = fields.Binary(string="Adjunto")
+    x_studio_adjunto = fields.Binary(string="Adjunto", attachment=True)
 
     @api.model
     def action_load_attachments(self):
         records = self.search([])
         for record in records:
+            # Buscar archivos adjuntos en los mensajes relacionados
             for message in record.message_ids:
-                attachment = message.attachment_ids[:1]
-                if attachment:
+                if message.attachment_ids:
+                    # Tomar el primer archivo adjunto encontrado y asignarlo al campo
+                    attachment = message.attachment_ids[0]
                     record.x_studio_adjunto = attachment.datas
+                    break  # Se detiene después de asignar el primer archivo encontrado
+
