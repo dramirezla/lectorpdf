fdb8f70895c2672829cd84f7360836ce8870cbac
jdquicenp <57123064+jdquicenp@users.noreply.github.com>
2024-11-28 00:27:12 -0500

    Update xrecepcionfacturas.py

--- RecepcionFacturas/models/xrecepcionfacturas.py
+++ RecepcionFacturas/models/xrecepcionfacturas.py
@@ -4,6 +4,9 @@ from odoo.exceptions import UserError
 class factPopup(models.Model):
     _name = 'recpfact'
     _description = 'Recep Fact'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    name = fields.Char(string="Nombre")
+    description = fields.Text(string="Descripción")
     def check_attachments(self):
         # Este método debería contener la lógica de comprobación de adjuntos
         attachments = self.env['mail.attachment'].search([
