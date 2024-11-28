de6b4a895c9a9dfe68d9f21f7a71972f1d6e5f5f
jdquicenp <57123064+jdquicenp@users.noreply.github.com>
2024-11-27 22:45:49 -0500

    Add files via upload

---
+++ RecepcionFacturas/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Recepción de Facturas',
+    'version': '2.0',
+    'author': 'www.fabricadeinnovacion.com',
+    'category': 'Accounting',
+    'summary': 'Procesar archivos adjuntos y crear facturas de compra.',
+    'depends': ['base', 'mail', 'account','studio_customization'],
+    'license': 'LGPL-3',
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+}
