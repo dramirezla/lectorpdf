8196a3f5d7b630082218c1d9c32c0fffb57a1005
jdquicenp <57123064+jdquicenp@users.noreply.github.com>
2024-11-27 19:14:49 -0500

    Update models.py

--- ArchivosAdjuntos/models/models.py
+++ ArchivosAdjuntos/models/models.py
@@ -1,3 +1,5 @@
+from odoo import models, fields, api  # Importación correcta de `models` desde Odoo
+
 class ListAttachmentLoader(models.Model):
     _name = "x_recepcion_facturas"
     _inherit = "x_recepcion_facturas"
@@ -8,13 +10,10 @@ class ListAttachmentLoader(models.Model):
     def action_load_attachments(self):
         records = self.search([])
         for record in records:
-            # Buscar mensajes relacionados al registro
             for message in record.message_ids:
-                if message.attachment_ids:
-                    # Asignar el primer archivo adjunto a x_studio_adjunto
-                    attachment = message.attachment_ids[0]
-                    if attachment:
-                        record.x_studio_adjunto = attachment.datas
-                        break  # Salir después de asignar el primer archivo
+                attachment = message.attachment_ids[:1]  # Tomamos el primer adjunto
+                if attachment:
+                    record.x_studio_adjunto = attachment.datas
+
 
 
