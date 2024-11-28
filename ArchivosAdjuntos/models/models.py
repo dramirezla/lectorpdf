472c6793f39df4db367d55bda143a9d22bde86ac
jdquicenp <57123064+jdquicenp@users.noreply.github.com>
2024-11-27 19:09:44 -0500

    Update models.py

--- ArchivosAdjuntos/models/models.py
+++ ArchivosAdjuntos/models/models.py
@@ -1,5 +1,4 @@
-import logging
-_logger = logging.getLogger(__name__)
+from odoo import models, fields, api
 
 class ListAttachmentLoader(models.Model):
     _name = "x_recepcion_facturas"
@@ -9,15 +8,9 @@ class ListAttachmentLoader(models.Model):
 
     @api.model
     def action_load_attachments(self):
-        records = self.search([])  # Considerar aplicar filtros si es necesario
+        records = self.search([])
         for record in records:
             for message in record.message_ids:
-                if message.attachment_ids:
-                    attachment = message.attachment_ids[0]  # Asegura que solo el primer adjunto se usa
-                    if attachment:
-                        record.x_studio_adjunto = attachment.datas
-                        _logger.info(f'Attachment loaded for record {record.id}')
-                    else:
-                        _logger.warning(f'No attachment found in message {message.id} for record {record.id}')
-                else:
-                    _logger.warning(f'No messages with attachments for record {record.id}')
+                attachment = message.attachment_ids[:1]
+                if attachment:
+                    record.x_studio_adjunto = attachment.datas
