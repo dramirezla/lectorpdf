b3c7fb540a995101a1bb8d3f4caaa05997216571
jdquicenp <57123064+jdquicenp@users.noreply.github.com>
2024-11-27 19:12:58 -0500

    Update models.py

--- ArchivosAdjuntos/models/models.py
+++ ArchivosAdjuntos/models/models.py
@@ -1,20 +1,20 @@
-from odoo import models, fields, api
-
 class ListAttachmentLoader(models.Model):
     _name = "x_recepcion_facturas"
     _inherit = "x_recepcion_facturas"
 
-    x_studio_adjunto = fields.Binary(string="Adjunto", attachment=True)
+    x_studio_adjunto = fields.Binary(string="Adjunto")
 
     @api.model
     def action_load_attachments(self):
         records = self.search([])
         for record in records:
-            # Buscar archivos adjuntos en los mensajes relacionados
+            # Buscar mensajes relacionados al registro
             for message in record.message_ids:
                 if message.attachment_ids:
-                    # Tomar el primer archivo adjunto encontrado y asignarlo al campo
+                    # Asignar el primer archivo adjunto a x_studio_adjunto
                     attachment = message.attachment_ids[0]
-                    record.x_studio_adjunto = attachment.datas
-                    break  # Se detiene después de asignar el primer archivo encontrado
+                    if attachment:
+                        record.x_studio_adjunto = attachment.datas
+                        break  # Salir después de asignar el primer archivo
+
 
