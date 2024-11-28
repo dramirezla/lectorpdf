56ec295bd730e417f3a813592b52b91e51d51086
jdquicenp <57123064+jdquicenp@users.noreply.github.com>
2024-11-27 20:50:53 -0500

    Update x_recepcion_facturas.py

--- Recepcion_Facturas/models/x_recepcion_facturas.py
+++ Recepcion_Facturas/models/x_recepcion_facturas.py
@@ -2,7 +2,7 @@ from odoo import models, fields, api
 from odoo.exceptions import UserError
 
 class XRecepcionFacturas(models.Model):
-    _name = 'x_recepcion_facturas'
+    _name = 'acount.move'
     _description = 'Recepción de Facturas'
 
     name = fields.Char(string='Nombre de la Factura')
