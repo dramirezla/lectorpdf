656b43de47542d74705bf1ab6e436a964f6ebda9
jdquicenp <57123064+jdquicenp@users.noreply.github.com>
2024-11-27 23:10:25 -0500

    Update xrecepcionfacturas.py

--- RecepcionFacturas/models/xrecepcionfacturas.py
+++ RecepcionFacturas/models/xrecepcionfacturas.py
@@ -2,7 +2,7 @@ from odoo import models, fields, api
 from odoo.exceptions import UserError
 
 class XRecepcionFacturas(models.Model):
-    _name = 'x_recepcion_facturas'
+    _inherit = 'x_recepcion_facturas'
     def check_attachments(self):
         # Este método debería contener la lógica de comprobación de adjuntos
         attachments = self.env['mail.attachment'].search([
