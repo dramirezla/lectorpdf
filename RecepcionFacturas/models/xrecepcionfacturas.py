10edd91beba5caf75db80eeae373a6bc11743720
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2024-12-04 21:49:26 -0500

    Update xrecepcionfacturas.py

--- RecepcionFacturas/models/xrecepcionfacturas.py
+++ RecepcionFacturas/models/xrecepcionfacturas.py
@@ -26,7 +26,7 @@ class RecepFact(models.Model):
         # Dividir el string en líneas
         lines = products_matrix.strip().split("\n")
         parsed_products = []
-        raise ValueError(f"Fila con formato incorrecto: {line}")
+        raise ValueError(f"{lineS}")
         
         for line in lines:
             # Dividir cada línea en columnas (ajusta según el separador)
